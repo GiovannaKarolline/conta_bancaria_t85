7b333b96f2cab140dd4f463e7ebff82fb807e5b7
Giovanna <mennezes.giovanna@gmail.com>
2026-06-20 22:36:04 -0300

    fix: adicionando switxh case da classe Menu

--- conta_bancaria/src/conta_bancaria/Menu.java
+++ conta_bancaria/src/conta_bancaria/Menu.java
@@ -13,8 +13,10 @@ public class Menu {
 		
 		while(true) {
 			System.out.println(Cores.ANSI_WHITE_BACKGROUND_BRIGHT  + Cores.TEXT_BLUE_BOLD+ "˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜");
-			System.out.println("                            Banco                            ");                         
+			System.out.println("                            Banco                            ");    
+			System.out.println("                                                             ");
 			System.out.println("˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜" + Cores.TEXT_PURPLE_BOLD);
+			System.out.println("                                                             ");
 			System.out.println("                   1 - Criar conta                           ");
 			System.out.println("                   2 - Listar todas as contas                ");
 			System.out.println("                   3 - Buscar conta por número               ");
@@ -23,9 +25,64 @@ public class Menu {
 			System.out.println("                   6 - Sacar                                 ");                   
 			System.out.println("                   7 - Depositar                             ");                   
 			System.out.println("                   8 - Transferir valores entre contas       ");
-			System.out.println("                   0 - Sair                                  ");                   
+			System.out.println("                   0 - Sair                                  ");
+			System.out.println("                                                             ");
 			System.out.println(Cores.TEXT_BLUE_BOLD + "˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜");
 			System.out.println("Digite a opção desejada:                                     ");
+			opcao = leia.nextInt();
+
+			if (opcao == 0) {
+				System.out.println(Cores.TEXT_BLUE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
 			}
+
+			switch (opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_BLUE + "Criar Conta\n\n");
+
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_BLUE + "Listar todas as Contas\n\n");
+
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_BLUE + "Consultar dados da Conta - por número\n\n");
+
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_BLUE + "Atualizar dados da Conta\n\n");
+
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_BLUE + "Apagar a Conta\n\n");
+
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_BLUE + "Saque\n\n");
+
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_BLUE + "Depósito\n\n");
+
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_BLUE + "Transferência entre Contas\n\n");
+
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+				break;
+			}
+		}
+	}
+
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: Giovanna Karolline");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("github.com/conteudoGeneration");
+		System.out.println("*********************************************************");
 		}
 	}
